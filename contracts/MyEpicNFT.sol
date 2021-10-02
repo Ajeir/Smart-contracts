e51e4df27bc93184ade916e3b270022cf9418871
Ajeir Cunningham <dev0sarc@gmail.com>
2021-10-02 16:25:43 -0500

    limit function

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -14,6 +14,7 @@ contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
   string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
@@ -48,17 +49,15 @@ contract MyEpicNFT is ERC721URIStorage {
     return thirdWords[rand];
   }
 
+ 
+
   function random(string memory input) internal pure returns (uint256) {
       return uint256(keccak256(abi.encodePacked(input)));
   }
 
-  function makeNFT() public {
-    uint256 newItemId = _tokenIds.current();
-    require(newItemId < 51, "50 NFTs have already been minted. No more are available."); 
-    }
-
   function makeAnEpicNFT() public {
     uint256 newItemId = _tokenIds.current();
+    require(newItemId < 51, "50 NFTs have already been minted. No more are available.");
     
     
 
